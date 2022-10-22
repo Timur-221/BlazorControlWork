0ab32ff66e3f2193d9743167e278eb52e0b0ee52
islamov302 <113845313+islamov302@users.noreply.github.com>
2022-10-22 17:34:05 +0300

    Add project files.

---
+++ BlazorApp1/Data/MD.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+
+
+namespace BlazorApp1.Data
+{
+    public class MD
+    {
+        private string connection = "mongodb://localhost:27017";
+        private IMongoDatabase database;
+
+        public MD()
+        {
+            var client = new MongoClient(connection);
+            database = client.GetDatabase("User");
+        }
+
+        public void AddUser(User user)
+        {
+            var collection = database.GetCollection<User>("Useres");
+            collection.InsertOne(user);
+
+        }
+    }
+}
---
+++ BlazorApp1/Data/User.cs
@@ -0,0 +1,16 @@
+﻿
+namespace BlazorApp1.Data
+{
+    public class User
+    {
+        public string Email { get; set; }
+
+        public string Login { get; set; }
+
+        public int Password { get; set; }
+
+        public string Name { get; set; }
+
+        public string FName { get; set; }
+    }
+}
